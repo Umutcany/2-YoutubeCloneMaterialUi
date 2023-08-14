a371357a3e0261fa472fcabebc6a85cf486e8a3c
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-08-14 16:58:06 +0300

    sidebarchange

--- youtubeClone/src/components/Feed.tsx
+++ youtubeClone/src/components/Feed.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack
       sx={{
@@ -19,14 +26,29 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2023 UmutCanYılmaz
+          Copyright @2023 Umut Can Yılmaz
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos />
       </Box>
     </Stack>
   );
